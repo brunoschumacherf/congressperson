64cfe37c9e23cce3a3e61b6308be237358c28942
Bruno Schumacher Farias Souza <brunoschumacherfsouza@gmail.com>
2022-08-14 22:11:30 -0300

    feat(NO-ISSUE): (assents precompile)

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Congressperson
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.assets.initialize_on_precompile = false
     config.load_defaults 6.1
     config.action_controller.include_all_helpers = false
     config.active_job.queue_adapter = :sidekiq
